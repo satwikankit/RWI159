d0f335cc3ecc8e60c1803841026066bab3f2f981
satwikankit <100881087+satwikankit@users.noreply.github.com>
2023-10-06 11:33:48 +0530

    Add files via upload
    Day:1 
    SawpingOfTwoNumber

---
+++ SwapingOfTwoNumber/SwapingOfTwoNumber.java
@@ -0,0 +1,16 @@
+class  SwapingOfTwoNumber
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World!");
+		int a = 10;
+		int b = 20;
+		int c = a;
+		a=b;
+		b=c;
+		System.out.println(a);
+		System.out.println(b);
+		
+
+	}
+}
