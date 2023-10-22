df0a97d3e0984493f8ae77ea5e3b1535c668f9db
satwikankit <100881087+satwikankit@users.noreply.github.com>
2023-10-22 20:18:40 +0530

    CustomClassPhoneLogicCode

---
+++ PhoneLogic/PhoneLogic.java
@@ -0,0 +1,24 @@
+class PhoneLogic
+	{
+		public void ring()
+			{
+			System.out.println("Ringing....");
+			
+		}
+		public void vibrating()
+			{
+			System.out.println("Vibrating....");
+			
+			
+		}
+	
+	public static void  main(String[] args) 
+	{
+		PhoneLogic samsung = new samsung();
+		
+	}
+	PhoneLogic samsung = new PhoneLogic();
+	 ring();
+	 virating();
+	}
+	
