5def292b10966b39af2d3edc6d0e5ec2489ffa71
satwikankit <100881087+satwikankit@users.noreply.github.com>
2023-11-07 01:19:58 +0530

    Company portfolio Web-Site
    Dummy Company Portfolio Web-Site

---
+++ Company.js
@@ -0,0 +1,34 @@
+// button Start
+
+var button = document.getElementById("mybutton");
+
+button.onclick = function(){
+    alert(`SENT`);
+}
+// button ends
+
+
+// nav-bar Section Starts
+const opt = document.getElementById("opt");
+const hamburger = document.getElementById("hamburger");
+Opt.onclick = function(){
+    if(opt.style.display === "none" || opt.style.display === " "){
+        opt.style.display = "flex";
+    }
+    else{
+        opt.style.display = "none";
+    }
+
+}
+
+// nav-bar Section ends
+
+
+
+
+
+   
+
+
+
+   
